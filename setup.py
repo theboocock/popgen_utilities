5d5a1080b58e90aaff510f5c4f31cc8906d1c495
James Boocock <james.boocock@ucla.edu>
2019-09-29 19:09:26 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='popgen_utils',
+      version='1.0',
+      description='Popgen utils used for Boocock et al 2019',
+      author='James Boocock',
+      author_email='theboocock@ucla.edu',
+      url='',
+      packages=['popgen_utils_bio'],
+     )
